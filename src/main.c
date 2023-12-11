4606ebdccdc76cb3089dadeca5983086aa2b39b5
Cinglair Capello <cinglair.llo@gmail.com>
2023-12-11 17:06:11 -0300

    second commit

--- src/main.c
+++ src/main.c
@@ -10,6 +10,8 @@
 #include <zephyr/sys/printk.h>
 #include <zephyr/sys/util.h>
 #include <zephyr/sys/__assert.h>
+#include <zephyr/shell/shell.h>
+#include <zephyr/zbus/zbus.h>
 #include <inttypes.h>
 
 #include <string.h>
@@ -30,6 +32,8 @@
 
 #define ARCH_STACK_PTR_ALIGN           8
 #define CONFIG_SYS_CLOCK_TICKS_PER_SEC 1000
+#define CONFIG_NUM_PREEMPT_PRIORITIES  15
+#define CONFIG_MAIN_STACK_SIZE         256
 
 #if !DT_NODE_HAS_STATUS(LED0_NODE, okay)
 #error "Unsupported board: led0 devicetree alias is not defined"
@@ -43,6 +47,38 @@
 #error "Unsupported board: sw0 devicetree alias is not defined"
 #endif
 
+ZBUS_CHAN_DEFINE(led_chan, /* Name */
+		 char,     /* Message type */
+
+		 NULL,                                 /* Validator */
+		 NULL,                                 /* User data */
+		 ZBUS_OBSERVERS(listener, subscriber), /* observers */
+		 ZBUS_MSG_INIT(0)                      /* Initial value */
+);
+
+static void listener_callback(const struct zbus_channel *chan)
+{
+	const char *led_msg = zbus_chan_const_msg(chan);
+	printk("Mensagem do LED: %c\n", *led_msg);
+}
+
+ZBUS_LISTENER_DEFINE(listener, listener_callback);
+
+ZBUS_SUBSCRIBER_DEFINE(subscriber, 4);
+
+static void subscriber_task(void)
+{
+	const struct zbus_channel *chan;
+
+	while (!zbus_sub_wait(&subscriber, &chan, K_FOREVER)) {
+		char led_msg;
+
+		if (&led_chan == chan) {
+			zbus_chan_read(&led_chan, &led_msg, K_MSEC(500));
+		}
+	}
+}
+
 struct led {
 	struct gpio_dt_spec spec;
 	uint8_t num;
@@ -53,11 +89,16 @@ static const struct led led0 = {
 	.num = 0,
 };
 
+static const struct led led1 = {
+	.spec = GPIO_DT_SPEC_GET_OR(LED1_NODE, gpios, {0}),
+	.num = 0,
+};
+
 static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET_OR(SW0_NODE, gpios, {0});
-static struct gpio_dt_spec led = GPIO_DT_SPEC_GET_OR(LED_NODE, gpios, {0});
 static struct gpio_callback buttonCbData;
 
 static int buttonPressCount = 0;
+static int zbusBlinkLed = 0;
 
 static gpio_callback_handler_t button_pressed()
 {
@@ -126,9 +167,31 @@ void blink(const struct led *led, uint32_t sleep_ms, uint32_t id)
 	}
 }
 
+static int cmd_led(const struct shell *shell, size_t argc, char **argv)
+{
+	ARG_UNUSED(argc);
+	ARG_UNUSED(argv);
+
+	shell_print(shell, "Ligar LED\n");
+	k_msleep(200);
+	char led_msg = '1';
+	zbus_chan_pub(&led_chan, &led_msg, K_SECONDS(1));
+	return 0;
+}
+
+SHELL_CMD_ARG_REGISTER(led, NULL, "Description: Pisca um Led", cmd_led, 1, 0);
+
 void blink0(void)
 {
+
 	blink(&led0, 200, 0);
+}
+
+void blink1(void)
+{
+	char msg = " ";
+	zbus_chan_read(&led_chan, &msg, K_MSEC(500));
+	blink(&led1, 200, 0);
 }
 
 void button0(void)
@@ -137,8 +200,13 @@ void button0(void)
 }
 
 K_THREAD_DEFINE(blink0_id, STACKSIZE, blink0, NULL, NULL, NULL, PRIORITY, 0, 0);
+K_THREAD_DEFINE(blink1_id, STACKSIZE, blink1, NULL, NULL, NULL, PRIORITY, 0, 0);
 K_THREAD_DEFINE(button0_thread, STACKSIZE, button0, NULL, NULL, NULL, PRIORITY, 0, 0);
+K_THREAD_DEFINE(subscriber_task_id, CONFIG_MAIN_STACK_SIZE, subscriber_task, NULL, NULL, NULL, 3, 0,
+		0);
 
+K_THREAD_DEFINE(subscriber_task_id1, CONFIG_MAIN_STACK_SIZE, subscriber_task, NULL, NULL, NULL, 3,
+		0, 0);
 int main(void)
 {
 	return 0;
