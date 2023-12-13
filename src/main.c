019aaba9c6f825cf1e87212a720efe66fb526c3a
Cinglair Capello <cinglair.llo@gmail.com>
2023-12-13 17:31:20 -0300

    subindo fft

--- src/main.c
+++ src/main.c
@@ -7,16 +7,17 @@
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
 #include <zephyr/drivers/gpio.h>
-#include <zephyr/sys/printk.h>
 #include <zephyr/sys/util.h>
 #include <zephyr/sys/__assert.h>
 #include <zephyr/shell/shell.h>
 #include <zephyr/zbus/zbus.h>
+#include <zephyr/logging/log.h>
 #include <inttypes.h>
 
 #include <string.h>
 
 #define ZEPHYR_USER_NODE DT_PATH(zephyr_user)
+LOG_MODULE_DECLARE(zbus, CONFIG_ZBUS_LOG_LEVEL);
 
 /* size of stack area used by each thread */
 #define STACKSIZE 1024
@@ -47,38 +48,6 @@
 #error "Unsupported board: sw0 devicetree alias is not defined"
 #endif
 
-ZBUS_CHAN_DEFINE(led_chan, /* Name */
-		 char,     /* Message type */
-
-		 NULL,                                 /* Validator */
-		 NULL,                                 /* User data */
-		 ZBUS_OBSERVERS(listener, subscriber), /* observers */
-		 ZBUS_MSG_INIT(0)                      /* Initial value */
-);
-
-static void listener_callback(const struct zbus_channel *chan)
-{
-	const char *led_msg = zbus_chan_const_msg(chan);
-	printk("Mensagem do LED: %c\n", *led_msg);
-}
-
-ZBUS_LISTENER_DEFINE(listener, listener_callback);
-
-ZBUS_SUBSCRIBER_DEFINE(subscriber, 4);
-
-static void subscriber_task(void)
-{
-	const struct zbus_channel *chan;
-
-	while (!zbus_sub_wait(&subscriber, &chan, K_FOREVER)) {
-		char led_msg;
-
-		if (&led_chan == chan) {
-			zbus_chan_read(&led_chan, &led_msg, K_MSEC(500));
-		}
-	}
-}
-
 struct led {
 	struct gpio_dt_spec spec;
 	uint8_t num;
@@ -98,13 +67,12 @@ static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET_OR(SW0_NODE, gpios, {
 static struct gpio_callback buttonCbData;
 
 static int buttonPressCount = 0;
-static int zbusBlinkLed = 0;
 
 static gpio_callback_handler_t button_pressed()
 {
 	buttonPressCount++;
-	printk("Botão apertado em %" PRIu32 "\n", k_cycle_get_32());
-	printk("Quantidade de vezes que o botão foi apertado %d\n", buttonPressCount);
+	LOG_INF("Botão apertado em %" PRIu32 "\n", k_cycle_get_32());
+	LOG_INF("Quantidade de vezes que o botão foi apertado %d\n", buttonPressCount);
 }
 
 void button_task()
@@ -113,15 +81,15 @@ void button_task()
 
 	// Verificar se o botão está pronto para uso
 	if (!gpio_is_ready_dt(&button)) {
-		printk("Erro: Botão %s não está pronto\n", button.port->name);
+		LOG_INF("Erro: Botão %s não está pronto\n", button.port->name);
 		return 0;
 	}
 
 	// Configuração do pino
 	ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
 	if (ret != 0) {
-		printk("Erro %d: Falha ao configurar o Botão %s no pino %d\n", ret,
-		       button.port->name, button.pin);
+		LOG_INF("Erro %d: Falha ao configurar o Botão %s no pino %d\n", ret,
+			button.port->name, button.pin);
 		return 0;
 	}
 
@@ -129,14 +97,14 @@ void button_task()
 	ret = gpio_pin_interrupt_configure_dt(&button, GPIO_INT_EDGE_TO_ACTIVE);
 
 	if (ret != 0) {
-		printk("Erro %d: Falha ao configurar a interrupção do Botão %s no pino %d\n", ret,
-		       button.port->name, button.pin);
+		LOG_INF("Erro %d: Falha ao configurar a interrupção do Botão %s no pino %d\n", ret,
+			button.port->name, button.pin);
 		return 0;
 	}
 
 	gpio_init_callback(&buttonCbData, button_pressed, BIT(button.pin));
 	gpio_add_callback(button.port, &buttonCbData);
-	printk("Botão configurado na porta %s no pino %d\n", button.port->name, button.pin);
+	LOG_INF("Botão configurado na porta %s no pino %d\n", button.port->name, button.pin);
 
 	k_msleep(SLEEP_TIME_MS);
 }
@@ -148,14 +116,14 @@ void blink(const struct led *led, uint32_t sleep_ms, uint32_t id)
 	int cnt = 0;
 
 	if (!device_is_ready(spec->port)) {
-		printk("Error: %s device is not ready\n", spec->port->name);
+		LOG_INF("Error: %s device is not ready\n", spec->port->name);
 		return;
 	}
 
 	ret = gpio_pin_configure_dt(spec, GPIO_OUTPUT);
 	if (ret != 0) {
-		printk("Error %d: failed to configure pin %d (LED '%d')\n", ret, spec->pin,
-		       led->num);
+		LOG_INF("Error %d: failed to configure pin %d (LED '%d')\n", ret, spec->pin,
+			led->num);
 		return;
 	}
 
@@ -174,23 +142,35 @@ static int cmd_led(const struct shell *shell, size_t argc, char **argv)
 
 	shell_print(shell, "Ligar LED\n");
 	k_msleep(200);
-	char led_msg = '1';
-	zbus_chan_pub(&led_chan, &led_msg, K_SECONDS(1));
 	return 0;
 }
 
 SHELL_CMD_ARG_REGISTER(led, NULL, "Description: Pisca um Led", cmd_led, 1, 0);
 
+// ************* COMANDOS DO ADC LIGA E DESLIGA *************
+
+static int cmd_demo_on(const struct shell *shell, size_t argc, char **argv)
+{
+	printk("ADC ligado");
+}
+static int cmd_demo_off(const struct shell *shell, size_t argc, char **argv)
+{
+	printk("ADC desligado");
+}
+SHELL_STATIC_SUBCMD_SET_CREATE(sub_demo, SHELL_CMD(on, NULL, "Print params command.", cmd_demo_on),
+			       SHELL_CMD(off, NULL, "Ping command.", cmd_demo_off),
+			       SHELL_SUBCMD_SET_END);
+SHELL_CMD_REGISTER(demo, &sub_demo, "Demo commands", NULL);
+
+// ************* ------------------------------- *************
+
 void blink0(void)
 {
-
 	blink(&led0, 200, 0);
 }
 
-void blink1(void)
+void blink1(struct k_work *)
 {
-	char msg = " ";
-	zbus_chan_read(&led_chan, &msg, K_MSEC(500));
 	blink(&led1, 200, 0);
 }
 
@@ -200,14 +180,46 @@ void button0(void)
 }
 
 K_THREAD_DEFINE(blink0_id, STACKSIZE, blink0, NULL, NULL, NULL, PRIORITY, 0, 0);
-K_THREAD_DEFINE(blink1_id, STACKSIZE, blink1, NULL, NULL, NULL, PRIORITY, 0, 0);
+K_WORK_DEFINE(blink1_id, blink1);
 K_THREAD_DEFINE(button0_thread, STACKSIZE, button0, NULL, NULL, NULL, PRIORITY, 0, 0);
-K_THREAD_DEFINE(subscriber_task_id, CONFIG_MAIN_STACK_SIZE, subscriber_task, NULL, NULL, NULL, 3, 0,
-		0);
 
-K_THREAD_DEFINE(subscriber_task_id1, CONFIG_MAIN_STACK_SIZE, subscriber_task, NULL, NULL, NULL, 3,
-		0, 0);
+/// TESTE DO EXEMPLO DO ZBUS
+
+ZBUS_CHAN_DEFINE(sensor_data_chan, char, NULL, NULL,
+		 ZBUS_OBSERVERS(thread_handler1_sub), /* observers */
+		 ZBUS_MSG_INIT(0)                     /* Initial value {0} */
+);
+
+void peripheral_thread(void)
+{
+	char sm = 't';
+
+	while (1) {
+		zbus_chan_pub(&sensor_data_chan, &sm, K_MSEC(200));
+		k_msleep(100);
+	}
+}
+
+K_THREAD_DEFINE(peripheral_thread_id, 1024, peripheral_thread, NULL, NULL, NULL, 5, 0, 0);
+
+ZBUS_SUBSCRIBER_DEFINE(thread_handler1_sub, 4);
+
+static void cmd_adc(const struct shell *shell)
+{
+	const struct zbus_channel *chan;
+	if (!zbus_sub_wait(&thread_handler1_sub, &chan, K_MSEC(200))) {
+		char msg;
+		zbus_chan_read(chan, &msg, K_MSEC(200));
+		shell_print(shell, "Valor: %c\n", msg);
+	}
+	return 0;
+}
+
+SHELL_CMD_ARG_REGISTER(adc, NULL, "Description: Gera sinal ADC", cmd_adc, 1, 0);
+/// ************************
+
 int main(void)
 {
+	k_work_submit(&blink1_id);
 	return 0;
 }
